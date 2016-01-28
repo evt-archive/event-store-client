326166cdccbcd5246a74fc16e492ce25b3b967db
Kelsey Isaacson <kelsey.isaacson@gmail.com>
2016-01-28 13:33:36 -0600

    Convert test framework from minitest to test_bench

--- test/spec/stream_name.rb
+++ test/spec/stream_name.rb
@@ -4,42 +4,42 @@ module Fixtures
   StreamName = EventStore::Client::StreamName
 end
 
-describe "Stream Name" do
+context "Stream Name" do
   stream_name = Fixtures::StreamName.stream_name 'someCategory', 'some_id'
 
-  specify "Composes the stream name from the category name and an ID" do
+  test "Composes the stream name from the category name and an ID" do
     assert(stream_name == 'someCategory-some_id')
   end
 end
 
-describe "Random Category Name" do
+context "Random Category Name" do
   stream_name = Fixtures::StreamName.random_category_name('someCategory')
 
-  specify "Adds a randomized suffix to the category name" do
+  test "Adds a randomized suffix to the category name" do
     assert(stream_name.length > 'someCategory'.length)
     assert(stream_name.start_with? 'someCategory')
   end
 end
 
-describe "Random Stream Name" do
+context "Random Stream Name" do
   stream_name = Fixtures::StreamName.stream_name 'someCategory', 'some_id', random: true
 
-  specify "Composes the stream name from the randomized category name and an ID" do
+  test "Composes the stream name from the randomized category name and an ID" do
     assert(stream_name.length > 'someCategory-some_id'.length)
     assert(stream_name.start_with? 'someCategory')
     assert(stream_name.end_with? 'some_id')
   end
 end
 
-describe "Category Stream Name" do
-  specify "Composes the category stream name from the category name and the reserve category stream prefix" do
+context "Category Stream Name" do
+  test "Composes the category stream name from the category name and the reserve category stream prefix" do
     category_stream_name = Fixtures::StreamName.category_stream_name 'someCategory'
     assert(category_stream_name == '$ce-someCategory')
   end
 end
 
-describe "Stream ID" do
-  specify "Is the UUID portion of a full stream name" do
+context "Stream ID" do
+  test "Is the UUID portion of a full stream name" do
     id = Identifier::UUID.random
     stream_name = "someStream-#{id}"
 
@@ -47,7 +47,7 @@ describe "Stream ID" do
     assert(stream_id == id)
   end
 
-  specify "Is nil if there is no type 4 UUID in the stream name" do
+  test "Is nil if there is no type 4 UUID in the stream name" do
     stream_id = Fixtures::StreamName.get_id 'someStream'
     assert(stream_id.nil?)
   end
